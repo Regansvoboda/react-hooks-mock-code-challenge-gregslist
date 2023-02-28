2fbfb6f9287c452fdb1e2804bbf1cc6b925be1cb
Regan Svoboda <regansvoboda@gmail.com>
2023-02-28 10:24:17 -0600

    cards

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, {useState, useEffect} from "react"
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
+// import ListingCard from "./ListingCard"
 
 function App() {
-  return (
-    <div className="app">
+  
+  const [card, setCard] = useState([])
+
+    useEffect(() => {
+      fetch(`http://localhost:6001/listings`)
+        .then((response) => response.json())
+        .then((data) => setCard(data))
+    }, []);
+console.log(card)
+  
+return (
+    <div className="app"> 
+    <div>{card.map((listing) => <p>{listing.desription}</p>)}</div>;
       <Header />
-      <ListingsContainer />
+      <ListingsContainer listings= {card} />
+      {/* <ListingCard description= {listing.desription} image= {listing.image} location= {listing.location} id= {listing.id}/>  */}
     </div>
-  );
-}
+   );
+  }
 
 export default App;
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,15 @@
 import React from "react";
-// import ListingCard from "./ListingCard";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+
+function ListingsContainer(props) {
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {props.listings.map((listing) => <ListingCard listing= {listing} image= {props.image}  ></ListingCard>)}
+    
+
+        
       </ul>
     </main>
   );
